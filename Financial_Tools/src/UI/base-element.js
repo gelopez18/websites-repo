5eab4d3c1518757fde71e393d411ef1c117e9750
gelopez18 <94268643+gelopez18@users.noreply.github.com>
2022-08-13 14:02:50 -0600

    navbar

---
+++ Financial_Tools/src/UI/base-element.js
@@ -0,0 +1,17 @@
+import 'https://code.jquery.com/jquery-3.6.0.min.js';
+window.$ = $;
+window.jQuery = jQuery;
+
+export class BaseElement{
+    constructor(){
+        this.element = null;
+    }
+    createElement(){
+        let s = this.getElementString();
+        this.element = $(s);
+    }
+    getElementString(){
+        throw new error('Please overRide this Methods');
+    }
+    enableJS()
+}
