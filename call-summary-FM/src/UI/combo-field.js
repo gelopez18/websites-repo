d041608278b98be04ff6d586afccc01a9635e403
gelopez18 <94268643+gelopez18@users.noreply.github.com>
2022-08-25 22:21:27 -0600

    combobox

--- call-summary-FM/src/UI/combo-field.js
+++ call-summary-FM/src/UI/combo-field.js
@@ -1,7 +1,20 @@
 import { BaseElement } from "./base-element";
 
+
 export class ComboField extends BaseElement{
-    constructor(){
-        super()
+    constructor(id, users){
+        super();
+        this.id=id;
+        this.user=users;
     }
+    getElementString(){
+
+     return`
+        <select class="form-select" aria-label="Default select example" id="${this.id}">
+        <option value="1"></option>\n
+                ${usersAdd}
+        </select>
+        `; 
+    }
+
 }
