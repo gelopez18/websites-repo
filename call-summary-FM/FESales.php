0e8a98ce7597bbb253a61741e5785949f6fa926b
gelopez18 <94268643+gelopez18@users.noreply.github.com>
2022-09-06 21:14:35 -0600

    Update FESales.php

--- call-summary-FM/FESales.php
+++ call-summary-FM/FESales.php
@@ -24,7 +24,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                 <div class="row">
                     <div class="col">
                         <label for="exampleInputEmail1" class="form-label">Type</label>
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" aria-label="Default select example" name="Type1">
                             <option selected="selected">Choose one</option>
                                 <?php
                                 // A sample product array
@@ -39,11 +39,11 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                     </div>
                 <div class="col">
                         <label for="exampleInputEmail1" class="form-label">Weight</label>        
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" aria-label="Default select example" name="weight1">
                             <option selected="selected">Choose one</option>
                                 <?php
                                 // A sample product array
-                                $products = array("2.5", "5", "6", "Camera");
+                                $products = array("2.5", "5", "6", "9","10", "13", "15", "17", "20", "30", "150");
                                 
                                 // Iterating through the product array
                                 foreach($products as $item){
@@ -54,14 +54,14 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                 </div>
                 <div class="col">
                     <label for="exampleInputEmail1" class="form-label">Qty</label>
-                    <input type="text" class="form-control" id="wkNumb" name="sro" aria-describedby="emailHelp">
+                    <input type="text" class="form-control" id="wkNumb" name="Qty1" aria-describedby="emailHelp">
                 </div>
             </div>
             <br>
             <div class="row">
                     <div class="col">
 
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" aria-label="Default select example" name="Type2">
                             <option selected="selected">Choose one</option>
                                 <?php
                                 // Iterating through the product array
@@ -73,7 +73,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                     </div>
                 <div class="col">
      
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" aria-label="Default select example" name="weight2">
                             <option selected="selected">Choose one</option>
                                 <?php
                                 // Iterating through the product array
@@ -85,17 +85,49 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                 </div>
                 <div class="col">
 
-                    <input type="text" class="form-control" id="wkNumb" name="sro" aria-describedby="emailHelp">
+                    <input type="text" class="form-control" id="wkNumb" name="Qty2" aria-describedby="emailHelp">
                 </div>
             </div>
             <br>
             <div class="row">
                     <div class="col">
 
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" aria-label="Default select example" name="Type3">
                             <option selected="selected">Choose one</option>
                                 <?php
                                 
+                                // Iterating through the product array
+                                foreach($products as $item){
+                                    echo "<option value='strtolower($item)'>$item</option>";
+                                }
+                                ?>
+                        </select>
+                    </div>
+                <div class="col">
+     
+                        <select class="form-select" aria-label="Default select example" name="weight3">
+                            <option selected="selected">Choose one</option>
+                                <?php
+                               
+                                // Iterating through the product array
+                                foreach($products as $item){
+                                    echo "<option value='strtolower($item)'>$item</option>";
+                                }
+                                ?>
+                        </select>
+                </div>
+                <div class="col">
+
+                    <input type="text" class="form-control" id="wkNumb" name="Qty" aria-describedby="emailHelp">
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                    <div class="col">
+
+                        <select class="form-select" aria-label="Default select example" name="Type4">
+                            <option selected="selected">Choose one</option>
+                                <?php
                                 // Iterating through the product array
                                 foreach($products as $item){
                                     echo "<option value='strtolower($item)'>$item</option>";
@@ -118,40 +150,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                 </div>
                 <div class="col">
 
-                    <input type="text" class="form-control" id="wkNumb" name="sro" aria-describedby="emailHelp">
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                    <div class="col">
-
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected="selected">Choose one</option>
-                                <?php
-                                
-                                // Iterating through the product array
-                                foreach($products as $item){
-                                    echo "<option value='strtolower($item)'>$item</option>";
-                                }
-                                ?>
-                        </select>
-                    </div>
-                <div class="col">
-     
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected="selected">Choose one</option>
-                                <?php
-                               
-                                // Iterating through the product array
-                                foreach($products as $item){
-                                    echo "<option value='strtolower($item)'>$item</option>";
-                                }
-                                ?>
-                        </select>
-                </div>
-                <div class="col">
-
-                    <input type="text" class="form-control" id="wkNumb" name="sro" aria-describedby="emailHelp">
+                    <input type="text" class="form-control" id="wkNumb" name="sro" aria-describedby="emailHelp" >
                 </div>
             </div>
             
