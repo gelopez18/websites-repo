f597fc06212d463bb56e8a4b8d7fbce11d398364
gelopez18 <94268643+gelopez18@users.noreply.github.com>
2022-09-13 16:15:08 -0600

    landing page to submit email

--- Financial-Tools/myapp/routes/index.js
+++ Financial-Tools/myapp/routes/index.js
@@ -3,5 +3,5 @@ var router = express.Router(); //instance of the express router
 let landing = require('../controllers/landing');//import the index from the folder created controllers*R(index comment)
 /* GET home page. */
 router.get('/', landing.get_landing); //we call the index from the instance of the object
-
+router.post('/', landing.submit_lead);
 module.exports = router;
