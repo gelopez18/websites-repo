d2171a3d7abb308eae3d516e40e8445227d53b81
gelopez18 <94268643+gelopez18@users.noreply.github.com>
2022-09-14 22:16:20 -0600

    sum total per page in sales summary problem fix

--- call-summary-FM/SalesSummaryQY.php
+++ call-summary-FM/SalesSummaryQY.php
@@ -5,18 +5,18 @@ require_once './includes/connectDB.php';
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     /*$clientClass = $_POST['ClientClass'];
     $wkNumb=$_POST['wkNumb'];*/
-    $clientClass = 'C';
-    $wkNumb=49;
-}
 
+}
+$clientClass = 'C';
+$wkNumb=40;
 $result_per_page=10;
 //QUERIES
 
-$sqlQuery = "SELECT SRO, total, client, clientClass, OverRide, overRidePercent FROM salessummaries WHERE wkNumb='$wkNumb' AND ClientClass='$wkNumb';";//REMEMBER TO CHANGE BACK TO THE VARIABLE
-$sqlSum = "SELECT SUM(total) FROM salessummaries WHERE wkNumb='$wkNumb' AND ClientClass='$wkNumb';";
+$sqlQuery = "SELECT SRO, total, client, clientClass, OverRide, overRidePercent FROM salessummaries WHERE wkNumb='$wkNumb' AND ClientClass='$clientClass';";//REMEMBER TO CHANGE BACK TO THE VARIABLE
+
 
 //send all the queries to the server. 
-$total = $server->query($sqlSum);
+
 
 $Result= $server->query($sqlQuery);
 $TotalofRec = mysqli_num_rows($Result);
@@ -31,8 +31,10 @@ if(!isset($_GET['page'])){
 //determinate the results per page - page1 1-10, page2 11-20, page3 21-30
 $thisPageFirstResult = ($page-1)*$result_per_page;
 
-$sqlQuery1 = "SELECT SRO, total, client, clientClass, OverRide, overRidePercent FROM salessummaries WHERE wkNumb='$wkNumb' AND clientClass='$wkNumb' LIMIT $thisPageFirstResult, $result_per_page;";//REMEMBER TO CHANGE BACK TO THE VARIABLE
+$sqlQuery1 = "SELECT SRO, total, client, clientClass, OverRide, overRidePercent FROM salessummaries WHERE wkNumb='$wkNumb' AND clientClass='$clientClass' LIMIT $thisPageFirstResult, $result_per_page;";
+$sqlSum = "SELECT SUM(total) FROM salessummaries WHERE wkNumb='$wkNumb' AND ClientClass='$clientClass'LIMIT $thisPageFirstResult, $result_per_page;";
 $result=mysqli_query($server, $sqlQuery1);
+$total = $server->query($sqlSum);
 //results
 if ($Result->num_rows > 0) {
 
@@ -43,17 +45,14 @@ if ($Result->num_rows > 0) {
         <div class='col'>
         <img class='' src='./img/Picture2.png'>
         </div>
-     
         <div class='col-5'>
             <table >
                 <tr id='wkInfo'>
                     <td>
-                        Summary ID <b>wk49-2022</b>
+                        Summary ID <b>wk$wkNumb-2022</b>
                         <hr>
                     </td>
-
                 </tr>
-                
                 <tr>
                     <td class=>
                     <i>If summary ID number is not provided, one will be assigned to your summary. It is suggested you provide your own unique reference number for easier tracking of your work. Reference numbers will reflect on your statements, therefore avoid repeating.<i>
@@ -61,10 +60,8 @@ if ($Result->num_rows > 0) {
                 </tr>
                 <tr>
                     <td>
-
                     </td>
                 </tr>
-              
             </table>
         </div>
         <div class='col'>
@@ -124,13 +121,38 @@ while($row = mysqli_fetch_array($result)){
 } else {
     echo "0 results";
   }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sales Summary</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+        #wkInfo{
+            border-collapse: collapse;
+        }
+    </style>
+  </head>
+  <body>
+ 
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
- while($row = mysqli_fetch_array($total)){
-    echo "</table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <?php
+        $Trry=[];
+        $sroQuery="SELECT total FROM salessummaries WHERE wkNumb='$wkNumb' AND ClientClass='$clientClass' LIMIT $thisPageFirstResult, $result_per_page;";
+        $TT=mysqli_query($server, $sroQuery);
+    
+            while($row=mysqli_fetch_array($TT)){
+                array_push($Trry, $row[0]);
+        }
+        echo "</table>
     <table class='table'>
             <tr>
             <td colspan='2'>Total</td>
-            <td><b>\$".$row[0]."</b></td>
+            <td><b>\$".array_sum($Trry)."</b></td>
             <td></td>
             <td colspan='4'><b>Preparer's Signature</b></td>
             <td></td>
@@ -155,31 +177,8 @@ while($row = mysqli_fetch_array($result)){
     </table>
     <b>*Split 1 and 2 represents % of partner's total commission, not percentage of value of job</b></div>
     ";
-
-  }
-
-
-
  $server->close();
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sales Summary</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <style>
-        #wkInfo{
-            border-collapse: collapse;
-        }
-    </style>
-  </head>
-  <body>
- 
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
     <br><br>
   
